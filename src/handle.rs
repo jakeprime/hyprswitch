725ef71022a2b8fcca7c202620cd505b5ef22fc0
Jake Prime <jake@jakeprime.com>
2026-02-14 14:11:38 +0000

    Revert "Ensure focused window is floating"
    This reverts commit 2de3a9aa738b4dcc85a02e0fdc8ba1508de56e19.
    
    It didn't quite work all the time, sometimes it would tile instead of
    float. And I've introduced a keyboard shortcut to float all windows now
    anyway.

--- src/handle.rs
+++ src/handle.rs
@@ -5,7 +5,7 @@ use hyprland::{
     data::{Client, Clients, Monitors, Workspaces},
     dispatch::{
         Dispatch,
-        DispatchType::{BringActiveToTop, FocusWindow, ToggleFloating, ToggleSpecialWorkspace, Workspace},
+        DispatchType::{BringActiveToTop, FocusWindow, ToggleSpecialWorkspace, Workspace},
         WindowIdentifier, WorkspaceIdentifierWithSpecial,
     },
     prelude::*,
@@ -246,9 +246,6 @@ pub async fn switch_async(next_client: &Client, dry_run: bool) -> Result<(), Hyp
             next_client.address.clone(),
         ))).await?;
         Dispatch::call_async(BringActiveToTop).await?;
-        if !next_client.floating {
-            Dispatch::call_async(ToggleFloating(None)).await?;
-        }
     }
 
     Ok(())
